cef72d46b2a47e0854912c23c6050f4b24f1dfe0
Faacundo Nesci <facundonesci2007@gmail.com>
2024-09-26 19:52:38 -0300

    agregacion de carpetas src y doc

---
+++ src/informacion_tateti.js
@@ -0,0 +1,12 @@
+class tableroDeTateti = {
+	constructor(nombre, tablero, reglas){
+	this.nombre = nombre 
+    this.tablero = tablero 
+    this.reglas = reglas
+
+	}
+
+}
+const tableroDeTateti = new TableroDeTateti('tateti, 100cm de ancho y 100cm de largo con 9 casilleros dentro, es un juego por turno y comienza primero el jugar 1 (la X) y sigue el jugar 2 (el O) y el primero que haga ters en raya gana (cruzado vertical o horizontal)')
+
+
